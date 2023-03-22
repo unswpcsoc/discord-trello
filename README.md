53920371b55397b45d6f95c8eb5fb65a5d6e5324
Vishnu <vpgit123@gmail.com>
2023-03-22 19:19:56 +1100

    webhook test file updated using discord webhook service

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # discord-trello
 
+
+
 # Trello Webhook
 
 A webhook that will automatically receive the latest updates from the Trello board, process them and send messages to the relevant channel/person.
@@ -7,5 +9,13 @@ A webhook that will automatically receive the latest updates from the Trello boa
 # Discord Trello Bot
 
 Interact with the Trello board through discord
+
+
+
+# Test Setup
+Configure a discord webhook in the required Discord channel and copy the webhook url into the URL variable.
+
+
+
 
 
--- src/test.py
+++ src/test.py
@@ -1,19 +1,30 @@
 """
-Test whether pcsoc server is compatible with trello webhook api
+Test whether the webhook server works
 """
 
 from flask import Flask, request, abort
-
+from discord import Webhook
+import aiohttp
+import asyncio
 
 app = Flask(__name__)
+
+# This is the discord webhook url
+URL = ""
+
+
+async def send_message():
+    async with aiohttp.ClientSession() as session:
+        wh = Webhook.from_url(URL, session=session)
+        await wh.send('Hello World From the Internet', username='Test')
 
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        # print(request.json)
+        print(request.json)
         try:
-            print("the webhook is working")
+            asyncio.run(send_message())
             return 'success', 200
         except Exception as E:
             print(E)
@@ -21,8 +32,6 @@ def webhook():
     else:
         abort(400)
 
+
 if __name__ == '__main__':
-    app.run()
-
-
-
+    app.run(port=9080)
